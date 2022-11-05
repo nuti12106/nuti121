d9283e509c064f3e2f31c1552d7434a93617ce69
nuti12106 <117533082+nuti12106@users.noreply.github.com>
2022-11-05 23:51:41 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var path = require('path');
+
+var visitors = 0;
+const fs = require('fs');
+
+app.use(express.static(__dirname + '/public'))
+const Users = require('./models/User');
+
+app.get('/', (req, res) => {
+    visitors = visitors + 1
+    console.log('user visted homepage', visitors)
+
+
+    var options = {
+        root: path.join(__dirname)
+    };
+    var fileName = 'index.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+var users = []
+
+fs.readFile('users.json', 'utf8', (err, jsonString) => {
+    const data = JSON.parse(jsonString)
+    users = data;
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
